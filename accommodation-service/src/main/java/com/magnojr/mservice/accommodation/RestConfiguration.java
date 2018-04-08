a328e9924265718ed907466095b4a5ecd7ed8722
magnojr <is.magnojr@gmail.com>
2018-04-07 23:13:23 -0300

    fix url in Processor for accommodation

--- accommodation-service/src/main/java/com/magnojr/mservice/accommodation/RestConfiguration.java
+++ accommodation-service/src/main/java/com/magnojr/mservice/accommodation/RestConfiguration.java
@@ -1,16 +1,16 @@
-//package com.magnojr.mservice.accommodation;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-//import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-//
-//@Configuration
-//public class RestConfiguration extends RepositoryRestConfigurerAdapter
-//{
-//
-//   @Override
-//   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config)
-//   {
-//      config.setMaxPageSize(10);
-//   }
-//}
+package com.magnojr.mservice.accommodation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RestConfiguration extends RepositoryRestConfigurerAdapter
+{
+
+   @Override
+   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config)
+   {
+      config.setMaxPageSize(15);
+   }
+}
